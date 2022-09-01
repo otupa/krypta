5e9ad4993204b259ea0325fbeccc9fb18c935647
otupa <tupa.sales@gmail.com>
2022-09-01 20:13:15 -0300

    Criando Cabeçalho

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-
-
-function App() {
-  return <h1>Sejá bem vindo!</h1>
-}
-
-export default App
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import Header from '../../components/Header/index.tsx'
+
+export default function App() {
+  return (
+    <Header/>
+  )
+}
